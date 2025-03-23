493bf3c6b66b5af1785612ed3804ae0a189330b5
Klingsterina <eyglo2001@gmail.com>
2025-03-23 15:06:12 +0000

    symple 404 síða byrt ef flokkur fannst ekki

--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -1,6 +1,8 @@
+import { QuestionsApi } from '@/api';
 import { Category } from '@/components/Category/Category';
 import Navigation from '@/components/Navigation/Navigation';
 import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
 
 export default async function CategoryPage({
@@ -9,6 +11,21 @@ export default async function CategoryPage({
   params: Promise<{ category: string }>;
 }) {
   const { category } = await params;
+  // Sækjum flokkargögnin með QuestionsApi
+  const api = new QuestionsApi();
+  const categoryData = await api.getCategory(category);
+
+  // Ef flokkurinn er ekki til, birtum við 404-síðu
+  if (!categoryData) {
+    return (
+      <div className='container'>
+        <h1>Error</h1>
+        <h1>404</h1>
+        <p>Flokkurinn {category} fannst ekki</p>
+        <Navigation />
+      </div>
+    )
+  }
   return (
     <div className='container'>
       <Navigation />
