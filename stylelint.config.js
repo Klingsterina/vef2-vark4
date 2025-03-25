8af8f36283f64da75c0f05852910b5c28fbb21b1
Klingsterina <eyglo2001@gmail.com>
2025-03-25 02:12:08 +0000

    laga stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,6 @@ module.exports = {
     extends: ['stylelint-config-standard'],
     rules: {
       // Hér geturðu bætt við eða breytt reglum
+      "media-feature-range-notation": "prefix"
     },
   };
